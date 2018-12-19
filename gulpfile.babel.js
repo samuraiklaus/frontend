222727d96dc5e749e347ce7d47b8f54d389d9342
samuraiklaus <samurai.klaus@yandex.ru>
2018-12-19 16:27:55 +0300

    add sass and es6

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,27 @@
 import {src, dest, watch, series} from 'gulp'
 import browserSync from 'browser-sync'
 import pug from 'gulp-pug'
+import sass from 'gulp-sass'
+import rename from 'gulp-rename'
+import autoprefixer from 'gulp-autoprefixer'
+import cleanCss from 'gulp-clean-css'
+import notify from 'gulp-notify'
+import uglify from 'gulp-uglify'
+import babel from 'gulp-babel'
+import concat from 'gulp-concat'
 
 const paths = {
   templates: {
     src: 'src/*.pug',
     dest: 'app/'
+  },
+  styles: {
+    src: 'src/sass/**/*.sass',
+    dest: 'app/css/'
+  },
+  scripts: {
+    src: 'src/js/**/*.js',
+    dest: 'app/js/'
   }
 }
 
@@ -17,6 +33,8 @@ export function server () {
     notify: false
   })
   watch(paths.templates.src, templates)
+  watch(paths.styles.src, styles)
+  watch(paths.scripts.src, scripts)
 }
 
 export function templates () {
@@ -26,7 +44,25 @@ export function templates () {
     .pipe(browserSync.stream())
 }
 
+export function styles () {
+  return src(paths.styles.src)
+    .pipe(sass({outputStyle: 'expanded'}).on('error', notify.onError()))
+    .pipe(autoprefixer())
+    .pipe(rename({suffix: '.min', prefix: ''}))
+    .pipe(cleanCss())
+    .pipe(dest(paths.styles.dest))
+    .pipe(browserSync.stream())
+}
 
-const build = series(templates, server)
+export function scripts () {
+  return src(paths.scripts.src, {sourcemaps: true})
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(concat('master.min.js'))
+    .pipe(dest(paths.scripts.dest))
+    .pipe(browserSync.stream())
+}
+
+const build = series(scripts, styles, templates, server)
 
 export default build
